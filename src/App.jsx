18ae6fb8fa76106baca60e0cd1e963f0042e0cc0
siddikur-dev <siddikur.dev@gmail.com>
2025-09-24 08:38:28 +0600

    footer upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import Footer from "./Component/Footer/Footer";
 import Navber from "./Component/Navber/Navber";
 
 function App() {
   return (
     <>
       <Navber></Navber>
+
+      <div>
+        <div className="bg-gray-500 rounded-lg">
+          <h2 className="text-3xl">Pending </h2>
+          <p>0</p>
+        </div>
+      </div>
+
+      <Footer></Footer>
     </>
   );
 }
